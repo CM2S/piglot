Fix wrong calculation of the loss for singleton responses

piglot/links.py
@@ -552,23 +552,20 @@ class LinksLoss:
         case_loss = case.loss.zero()
         for field, reference in case.fields.items():
             ref_x = reference[:,0]
-            ref_y = np.squeeze(reference[:,1:])
+            ref_y = reference[:,1:]
+            field_loss = case.loss.zero()
             try:
-                # Compute loss for this case: check if single or multiple dimensions on y
+                # Compute loss for this case
                 field_data = field.get(input_file)
                 field_x = field_data[:,0]
-                field_y = np.squeeze(field_data[:,1:])
-                if len(ref_y.shape) == 1:
-                    case_loss = case.loss.sum(case.loss(ref_x, field_x, ref_y, field_y), case_loss)
-                    responses[field.name()] = list(zip([float(a) for a  in field_x],
-                                                       [float(a) for a  in field_y]))
-                else:
-                    for i in range(0, ref_y.shape[1]):
-                        case_loss = case.loss.sum(case.loss(ref_x, field_x, ref_y[:,i], field_y[:,i]), case_loss)
-                        responses[field.name(i)] = list(zip([float(a) for a  in field_x],
-                                                            [float(a) for a  in field_y[:,i]]))
+                field_y = field_data[:,1:]
+                for i in range(0, ref_y.shape[1]):
+                    field_loss = case.loss.sum(case.loss(ref_x, field_x, ref_y[:,i], field_y[:,i]), field_loss)
+                    responses[field.name(i)] = list(zip([float(a) for a in field_x],
+                                                        [float(a) for a in field_y[:,i]]))
             except:
-                case_loss = case.loss.sum(case.loss.max_value(ref_x, ref_y), case_loss)
+                field_loss = case.loss.sum(case.loss.max_value(ref_x, ref_y), field_loss)
+            case_loss = case.loss.sum(field_loss, case_loss)
         final_loss = case.loss.scale(case_loss, 1.0 / len(case.fields))
         # Final touches on case history and file writing
         if self.cases_hist: