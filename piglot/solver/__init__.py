c0a2297f3dd25a3c2793b38d34be4a827ad60465
FranciscaAlves <a.francisca.c.alves@gmail.com>
2024-02-19 14:17:39 +0000

    Add crate interface, examples and documentation

--- piglot/solver/__init__.py
+++ piglot/solver/__init__.py
@@ -4,11 +4,13 @@ from piglot.parameter import ParameterSet
 from piglot.solver.solver import Solver
 from piglot.solver.links.solver import LinksSolver
 from piglot.solver.curve.solver import CurveSolver
+from piglot.solver.crate.solver import CRATESolver
 
 
 AVAILABLE_SOLVERS: Dict[str, Type[Solver]] = {
     'links': LinksSolver,
     'curve': CurveSolver,
+    'crate': CRATESolver,
 }
 
 
