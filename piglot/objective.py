d48d5adb1e269cee4d361c0a2e4ef766ff9b9024
Tiago Pires <tiago.pires@outlook.pt>
2024-07-31 14:07:30 +0100

    Implemented a step to guarante numerical stability of the STCH

--- piglot/objective.py
+++ piglot/objective.py
@@ -241,8 +241,23 @@ class ObjectiveResult:
                     ideal_point = np.where(types, 1.0, 0.0)
                     # Smoothing parameter for STCH
                     u = 0.01
-                    # Calculate the Tchebycheff function value
-                    tch_values = (np.abs((norm_funcs - ideal_point) * costs) / u) * weights
+                    # Precompute the numerator of Tchebycheff function value
+                    tch_numerator = np.abs((norm_funcs - ideal_point) * costs) * weights
+                    # Calculate the initial Tchebycheff function value
+                    tch_values = tch_numerator / u
+                    # Calculate the initial softmax value
+                    softmax = np.exp(tch_values)/(np.exp(tch_values).sum())
+                    # Parameters to ensure numerical stability
+                    u_increment = u  # Increment value for u
+                    max_u = 0.2  # Maximum value for u
+                    max_iterations = max_u/u  # Limit to prevent infinite loop
+                    for iteration in range(int(max_iterations)):
+                        if not np.any(softmax > (1 - 1e-4)):
+                            break
+                        u += u_increment
+                        tch_values = tch_numerator / u
+                        softmax = np.exp(tch_values)/(np.exp(tch_values).sum())
+                    # Return the final scalarisation value
                     return np.log(np.sum(np.exp(tch_values))) * u
                 return np.sum((norm_funcs*costs)*weights)
             return np.mean(self.values)
