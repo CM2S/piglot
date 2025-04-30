f725fd08edf2e2ca8c268cecf39404317c098348
Rui Coelho <ruicoelhopedro@gmail.com>
2025-04-30 16:12:45 +0100

    Refactor to use `subprocess.run`

--- piglot/__init__.py
+++ piglot/__init__.py
@@ -11,21 +11,20 @@ __version__ = '0.5.1'
 import os
 import subprocess
 try:
-    popen = subprocess.Popen(
+    result = subprocess.run(
         ['git', 'describe', '--tags'],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         cwd=os.path.dirname(__file__),
+        text=True,
+        check=True
     )
-except FileNotFoundError:
-    pass
-else:
-    stdout, stderr = popen.communicate()
-    if popen.returncode == 0:
-        __new_version__ = stdout.decode('utf-8').strip()
+    __new_version__ = result.stdout.strip()
 
-        # The Git tag format is expected to start with 'v' followed by the version number
-        # (e.g., 'v0.5.1'). This ensures that the tag corresponds to the current version defined
-        # in __version__.
-        if __new_version__.startswith(f'v{__version__}'):
-            __version__ = __new_version__[1:]
+    # The Git tag format is expected to start with 'v' followed by the version number
+    # (e.g., 'v0.5.1'). This ensures that the tag corresponds to the current version defined
+    # in __version__.
+    if __new_version__.startswith(f'v{__version__}'):
+        __version__ = __new_version__[1:]
+except (FileNotFoundError, subprocess.CalledProcessError):
+    pass
