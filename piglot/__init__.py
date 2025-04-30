27885a149244d2c25f6e5a5203f411a67c24f092
Rui Coelho <ruicoelhopedro@gmail.com>
2025-04-30 15:57:53 +0100

    Introduce versioning based on git tags

--- piglot/__init__.py
+++ piglot/__init__.py
@@ -5,3 +5,23 @@ __author__ = 'CM2S'
 __copyright__ = '2025, CM2S'
 __license__ = 'MIT'
 __version__ = '0.5.1'
+
+
+# Try to find the piglot version from the git tag
+import os
+import subprocess
+try:
+    popen = subprocess.Popen(
+        ['git', 'describe', '--tags'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd=os.path.dirname(__file__),
+    )
+except FileNotFoundError:
+    pass
+else:
+    stdout, stderr = popen.communicate()
+    if popen.returncode == 0:
+        __new_version__ = stdout.decode('utf-8').strip()
+        if __new_version__.startswith(f'v{__version__}'):
+            __version__ = __new_version__[1:]
